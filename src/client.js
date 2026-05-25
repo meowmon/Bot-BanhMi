b47a18eeb8b3ae79a0fad99dca17392622ed7c08
HieuTX <hieu.tx1@mor.com.vn>
2026-05-25 11:26:16 +0700

    update chat feature

--- src/client.js
+++ src/client.js
@@ -6,7 +6,6 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.MessageContent,
   ],
   partials: [Partials.Message, Partials.Reaction, Partials.User],
 });
